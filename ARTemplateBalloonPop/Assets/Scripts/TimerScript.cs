75b5ddbf752cad0fa1b3673a9ab633a3efa22001
JENkt4k <james.e.nelson@gmail.com>
2022-03-14 01:02:08 -0500

    working android close

--- ARTemplateBalloonPop/Assets/Scripts/TimerScript.cs
+++ ARTemplateBalloonPop/Assets/Scripts/TimerScript.cs
@@ -52,4 +52,10 @@ public class TimerScript : MonoBehaviour
 
         scoreText.text = 0 + "";
     }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+    // Application.Quit();
 }
